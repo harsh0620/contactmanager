ecdcb0126f60a15719f81fd1f49c31e66270c7e7
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2020-08-07 10:36:33 +0530

    Updates

---
+++ precache-manifest.3c435ab04e53b5b726ee59aabd772bb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fdd37e4d44829d7259bd55407c6437",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "297159ac119ecc5cc1eb",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ea42483bdfee5d242410",
+    "url": "/contactmanager/static/css/main.8b696ae3.chunk.css"
+  },
+  {
+    "revision": "297159ac119ecc5cc1eb",
+    "url": "/contactmanager/static/js/2.f7f3dff5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.f7f3dff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea42483bdfee5d242410",
+    "url": "/contactmanager/static/js/main.d6425ac0.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
